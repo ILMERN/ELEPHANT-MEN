7ccde0de3dac4c6690fa04c2fc6a0aec44fb5f39
ILMERN <damien.evil.cool@gmail.com>
2026-04-17 15:15:18 +0930

    16/04/2026 - Implemented some encryption so cheating is no longer trivial
    17/04/2025
    - Tried to implement some redundancy to prevent desync but just broke everything instead (a problem for tomorrow's me to solve)
    - Nvm looks like I fixed it

--- netManagement.js
+++ netManagement.js
@@ -29,12 +29,17 @@ let peer = null;
 let otherPeer = null;
 let peerID = null;
 let myActions = [];
-let iSent = false;
+let myPreviousActions = [];
+let myPreviousKey = 0;
+let myKey = 0;
+let iSent = 0; // 0 = unsent, 1 = hash sent, 2 = actions sent
 let theirActions = [];
-let theySent = false;
+let theirHash = null;
+let theySent = 0;
 let iRematch = false;
 let theyRematch = false;
 let pingCleared = true;
+let cancelNextPing = false;
 
 // Create systems for generating peer IDs
 const gameCode = 'ELMEN_';
@@ -43,6 +48,18 @@ function generateID() {
     return Array.from({ length: codeLength }, () =>
         chars[Math.floor(Math.random() * chars.length)]
     ).join('');
+}
+
+// Hash function
+function createHash(actions, key) {
+    const payload = `${key}${actions}`
+    let hash = 0;
+    for (let i = 0; i < payload.length; i++) {
+        let chr = payload.charCodeAt(i);
+        hash = (hash << 5) - hash + chr;
+        hash |= 0;
+    }
+    return hash;
 }
 
 // Host code
@@ -156,7 +173,70 @@ newOpponentButton.addEventListener('click', function () {
 });
 
 function handleData(data) {
-    if (data.type === "rematch") {
+    if (data.type === "hash") {
+        cancelNextPing = true;
+        if (otherPeer !== null && theySent === 0) {
+            theirHash = data.contents;
+            theySent = 1;
+        }
+    }
+
+    else if (data.type === "actions") {
+        if (otherPeer !== null) {
+            cancelNextPing = true;
+            if (theySent === 0) {
+                theirActions = data.contents[0];
+                theySent = 2;
+                switch (iSent) {
+                    case 0: break;
+                    case 1:
+                        otherPeer.send({ type: "actions", contents: [myActions, myKey] });
+                    case 2:
+                        executeActions([...myActions, ...theirActions], baseBoard);
+                        resetInputActions();
+                }
+            } else if (theySent === 1) {
+                let checkHash = createHash(data.contents[0], data.contents[1]);
+                if (checkHash === theirHash) {
+                    theirActions = data.contents[0];
+                    theySent = 2;
+                    switch (iSent) {
+                        case 0: break;
+                        case 1:
+                            otherPeer.send({ type: "actions", contents: [myActions, myKey] });
+                        case 2:
+                            executeActions([...myActions, ...theirActions], baseBoard);
+                            resetInputActions();
+                    }
+                }
+            }
+        }
+    }
+
+    else if (data.type === "ping") {
+        if (otherPeer !== null && !cancelNextPing) {
+            if (data.contents !== iSent) {
+                switch (iSent) {
+                    case 0:
+                        otherPeer.send({ type: "actions", contents: [myPreviousActions, myPreviousKey] });
+                        break;
+                    case 1:
+                        otherPeer.send({ type: "hash", contents: createHash(myActions, myKey) });
+                        break;
+                    case 2:
+                        otherPeer.send({ type: "actions", contents: [myActions, myKey] });
+                        break;
+                }
+            }
+            otherPeer.send({ type: "pong", contents: null });
+        }
+    }
+
+    else if (data.type === "pong") {
+        pingCleared = true;
+    }
+
+    else if (data.type === "rematch") {
         if (!theySent) {
             theyRematch = true;
             if (iRematch) {
@@ -168,23 +248,6 @@ function handleData(data) {
                 rematchNotification.innerHTML = "Opponent wants a rematch!";
             }
         }
-    } else if (data.type === "actions") {
-        if (otherPeer !== null) {
-            if (!theySent) {
-                theirActions = data.contents;
-                theySent = true;
-                if (iSent) {
-                    executeActions([...myActions, ...theirActions], baseBoard);
-                    resetInputActions();
-                }
-            }
-        }
-    } else if (data.type === "ping") {
-        if (otherPeer !== null) {
-            otherPeer.send({ type: "pong", contents: null });
-        }
-    } else if (data.type === "pong") {
-        pingCleared = true;
     }
 }
 
@@ -206,30 +269,43 @@ function enableNetUI() {
 
 // initialize execute send button and action input fields
 sendButton.addEventListener('click', function () {
+    cancelNextPing = true;
     if (peerID === null && otherPeer === null) {
         console.log('no connection!');
         return;
     }
-    if (!iSent) {
+    if (iSent === 0) {
         disableBoard();
         currentActions.forEach((set, i) => {
             myActions.push(...set);
         });
-        otherPeer.send({type: "actions", contents: myActions });
-        iSent = true;
-
-        if (theySent) {
-            executeActions([...theirActions, ...myActions], baseBoard);
-            resetInputActions();
+        switch (theySent) {
+            case 0:
+                myKey = Math.random();
+                otherPeer.send({ type: "hash", contents: createHash(myActions, myKey) });
+                iSent = 1;
+                break;
+            case 1:
+                otherPeer.send({ type: "actions", contents: [myActions, myKey] });
+                iSent = 2;
+                break;
+            case 2:
+                otherPeer.send({ type: "actions", contents: [myActions, myKey] });
+                executeActions([...theirActions, ...myActions], baseBoard);
+                resetInputActions();
+                break;
         }
     }
 });
 
 function resetInputActions() {
+    myPreviousActions = myActions;
+    myPreviousKey = myKey;
     myActions = [];
-    iSent = false;
+    iSent = 0;
     theirActions = [];
-    theySent = false;
+    theirHash = null;
+    theySent = 0;
     deleteAllInputActions();
     currentPhase = 0;
 };
@@ -240,12 +316,16 @@ function resetNetVariables() {
     otherPeer = null;
     peerID = null;
     myActions = [];
-    iSent = false;
+    myPreviousActions = [];
+    myPreviousKey = 0;
+    iSent = 0;
     theirActions = [];
-    theySent = false;
+    theirHash = null;
+    theySent = 0;
     iRematch = false;
     theyRematch = false;
     pingCleared = true;
+    cancelNextPing = false;
 
     hostCode.innerHTML = Array(codeLength).fill("-").join('');
 }
@@ -285,11 +365,18 @@ function attemptReconnect(attemptsRemaining) {
 
 function startPinging() {
     if (connectMode !== "none") {
-        if (pingCleared) {
-            otherPeer.send({ type: "ping", contents: null });
+        if (cancelNextPing) {
+            cancelNextPing = false;
+            setTimeout(startPinging, pingTimer);
+        }
+
+        else if (pingCleared) {
+            otherPeer.send({ type: "ping", contents: theySent });
             pingCleared = false;
             setTimeout(startPinging, pingTimer);
-        } else {
+        }
+
+        else {
             attemptReconnect(reconnectAttempts);
         }
     }
